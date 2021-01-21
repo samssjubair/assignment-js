e447b9d8b7812b3ec3652eb80b943491d5201c3a
samssjubair <samssjubair@gmail.com>
2021-01-21 22:52:21 +0600

    kilometer to meter function added

---
+++ assignment.js
@@ -0,0 +1,27 @@
+// https://github.com/samssjubair/assignment-js
+
+// ------------------------------------------------Kilometer To Meter-------------------------------------------
+
+function kilometerToMeter(km){
+    if(km>0){
+        return km*1000;
+    }
+    else{
+        return "Kilometer (Distance) must be a positive value";
+    }
+}
+
+
+// ------------------------------------------------Budget Calculator-------------------------------------------
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------Function Checker-------------------------------------------
+
+console.log(kilometerToMeter(-2));
